ef1cd958d90df7321c580d263342c1db87603d6a
brendasalenave <brendasalenave@live.com>
2017-04-09 17:43:04 -0300

    Consumidor

--- Consumidor.java
+++ Consumidor.java
@@ -1,19 +1,24 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Consumidor {
   public static void main(String[] args) {
     try{
       byte[] dado = new byte[256];
-      ip = InetAddress.getByName("192.168.108.128");
-                                                                        "ip, porta"
+      InetAddress ip = InetAddress.getByName("192.168.108.128");
+                                                                    //"ip, porta"
       DatagramPacket pacoteEnvio = new DatagramPacket(dado, dado.length, ip, 50001);
-      socket = new DatagramSocket(); // criação do endpoint
+      DatagramSocket socket = new DatagramSocket(); // criação do endpoint
       socket.send(pacoteEnvio);
       DatagramPacket pacoteRecebido = new DatagramPacket(dado, dado.length);
       socket.receive(pacoteRecebido);
       dado = pacoteRecebido.getData();
       String valor = new String(pacoteRecebido.getData());
       int valorInteiro = Integer.parseInt( valor.trim() );
-      somatorio += valorInteiro;
-    }catch(Exception e){
+      //somatorio += valorInteiro;
+    } catch(Exception e){
       System.out.println("Erro");
+    }
   }
 }
